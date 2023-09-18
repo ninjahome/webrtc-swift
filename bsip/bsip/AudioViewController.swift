33298ef7bc01866fec812b8ec349f499e1021d1b
basdevelop <hyperorchid@outlook.com>
2023-09-18 17:16:55 +0800

    save tmp

--- bsip/bsip/AudioViewController.swift
+++ bsip/bsip/AudioViewController.swift
@@ -94,18 +94,20 @@ class AudioViewController: UIViewController, WebrtcLibCallBackProtocol {
                         return
                 }
                 WebrtcLibSetAnswerForOffer(sdp)
-                //                var err:NSError?
-                //                WebrtcLibAnswerVideo(sdp, self, &err)
-                //                if let e = err{
-                //                        print("------>>>start failed:",e.localizedDescription)
-                //                }
         }
+        
         @IBAction func StartEcho(_ sender: UIButton) {
+#if false
                 var err:NSError?
                 WebrtcLibStartVideo(true, self, &err)
                 if let e = err{
                         print("------>>>start failed:",e.localizedDescription)
                 }
+#else
+                engine.prepare()
+                try! engine.start()
+                player.play()
+#endif
         }
         
         private func initEngine(){
@@ -123,67 +125,13 @@ class AudioViewController: UIViewController, WebrtcLibCallBackProtocol {
                         
                         let bus = 0
                         let inputFormat = input.inputFormat(forBus: bus)
-                        print("------>>>channel number:\(inputFormat.channelCount)")
                         
-                        var opusASBD = AudioStreamBasicDescription(mSampleRate: 48000.0,
-                                                                   mFormatID: kAudioFormatOpus,
-                                                                   mFormatFlags: 0,
-                                                                   mBytesPerPacket: 0,
-                                                                   mFramesPerPacket: 2880,
-                                                                   mBytesPerFrame: 0,
-                                                                   mChannelsPerFrame: 1,
-                                                                   mBitsPerChannel: 0,
-                                                                   mReserved: 0)
-                        
-                        let recordingFormat = AVAudioFormat(streamDescription: &opusASBD)!
                         engine.connect(player, to: engine.outputNode, format: inputFormat)
                         
-                        guard let converter = AVAudioConverter(from: inputFormat, to: recordingFormat) else{
-                                print("------>>> no valid converter")
-                                return
-                        }
+                        
                         input.installTap(onBus: bus, bufferSize: 4096, format: inputFormat) { (buffer, time) -> Void in
                                 self.conversionQueue.async {
-                                        let pcmBuffer = AVAudioPCMBuffer(pcmFormat: recordingFormat, frameCapacity: AVAudioFrameCount(recordingFormat.sampleRate * 2.0))
-                                        
-                                        var err:NSError?
-                                        let inputBlock: AVAudioConverterInputBlock = {inNumPackets, outStatus in
-                                                outStatus.pointee = AVAudioConverterInputStatus.haveData
-                                                return buffer
-                                        }
-                                        converter.convert(to: pcmBuffer!, error: &err, withInputFrom: inputBlock)
-                                        if let e = err {
-                                                print("------>>>convert failed:",e.localizedDescription)
-                                                return
-                                        }
-                                        
-                                        guard let convertedBuffer = pcmBuffer else{
-                                                print("------>>>converted pcm buffer is empty:")
-                                                return
-                                        }
-                                        let data = audioBufferToNSData(PCMBuffer: convertedBuffer)
-//                                        if let channelData = pcmBuffer!.int16ChannelData {
-//
-//                                                let channelDataValue = channelData.pointee
-//                                                let channelDataValueArray = stride(from: 0,
-//                                                                                   to: Int(pcmBuffer!.frameLength),
-//                                                                                   by: buffer.stride).map{ channelDataValue[$0] }
-//
-//                                                // Converted pcm 16 values are delegated to the controller.
-//                                                Data(channelDataValueArray)
-//                                                self.delegate?.didOutput(channelData: channelDataValueArray)
-//                                                // completion(channelDataValueArray)
-//                                        }
-                                        
-                                        
-//                                        let data = audioBufferToNSData(PCMBuffer: buffer)
-                                        
-                                        WebrtcLibSendAudioToPeer(data, &err)
-                                        if let e = err{
-                                                print("------>tap err:",e.localizedDescription)
-                                        }
-                                        //                                let b2 = toPCMBuffer(data: data as NSData)
-                                        //                                self.player.scheduleBuffer(b2)
+                                        self.player.scheduleBuffer(buffer)
                                 }
                         }
                         
